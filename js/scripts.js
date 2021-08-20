7d69333e6fe3aa21c82b65fd64a9f83425fc08c4
toddmac12 <toddmac12@gmail.com>
2021-08-20 19:32:36 -0400

    deleted some comments that were used for reference on loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,5 @@
 // Business Logic
 
-//JavaScript Demo: Statement - If...Else
-
-// function testNum(a) {
-//   let result;
-//   if (a > 0) {
-//     result = 'positive';
-//   } else {
-//     result = 'NOT positive';
-//   }
-//   return result;
-// }
-// for ([initialExpression]; [conditionExpression]; [incrementExpression])
-//   statement
 function numberRoboNumber(number) {
   let numberUserOutput = [];
   for (let i = 0; i <= number; i++) {
