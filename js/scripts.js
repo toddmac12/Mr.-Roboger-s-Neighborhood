a8cbccc99432569a555e51ebdf35943675954bff
toddmac12 <toddmac12@gmail.com>
2021-08-20 17:39:56 -0400

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,29 @@
 // }
 // for ([initialExpression]; [conditionExpression]; [incrementExpression])
 //   statement
-function numberRoboNumber(number){
-let numberUserOutput = [];
-for (let i=0; i<=number;i++){
-if (i.toString().includes(3)) {
-numberUserOutput.push("Won't you be my neighbor?");} else if (i.toString().inclues(2)){
-numberUserOutput.push("Boop !");
-}else if (i.toString().includes(1)){
-numberUserOutput.push("Beep !");
-} else {
-numberUserOutput.push(i);
-}
-};
-return numberUserOutput;
+function numberRoboNumber(number) {
+  let numberUserOutput = [];
+  for (let i = 0; i <= number; i++) {
+    if (i.toString().includes(3)) {
+      numberUserOutput.push("Won't you be my neighbor?");
+    } else if (i.toString().inclues(2)) {
+      numberUserOutput.push("Boop !");
+    } else if (i.toString().includes(1)) {
+      numberUserOutput.push("Beep !");
+    } else {
+      numberUserOutput.push(i);
+    }
+  };
+  return numberUserOutput;
 };
 
 //User Logic
+
+$(document).ready(function () {
+  $("#calculate").submit(function (event) {
+    event.preventDefault();
+    let number = $("#numberInput").val();
+    let output = numberRoboNumber(number);
+    $("numberOutput").text(output);
+  });
+});
